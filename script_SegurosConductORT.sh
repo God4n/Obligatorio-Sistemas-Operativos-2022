a175bb95b054743d37c7dca70ec9db4f1941da80
TadeoGoday <tadeo.goday@gmail.com>
2022-09-07 09:00:42 -0300

    FIX: Detalles del menu

---
+++ script_SegurosConductORT.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+#Funcion para: Registrar Matriculas
+Registrar_Matricula(){
+        echo; echo "Registrar Matricula"
+	echo "Funcion 1"; echo;
+	
+}
+
+#Funcion para: Ver Matriculas Registradas
+Ver_Matriculas_Registradas(){
+	echo; echo "Ver Matriculas Registradas"
+        echo "Funcion 2"; echo;
+	
+}
+
+#Funcion para: Buscar Matriculas por Usuario
+Buscar_Matriculas_por_Usuario(){
+        echo; echo "Buscar Matriculas por Usuario"
+	echo "Funcion 3"; echo;
+	
+}
+
+#Funcion para: Cambiar Permiso de Modificacion
+Cambiar_Permiso_de_Modificacion(){
+        echo; echo "Cambiar Permiso de Modificacion"
+	echo "Funcion 4"; echo;
+	
+}
+
+while [ true ]; do
+	clear
+	figlet --gay -t -k "Seguros ConductORT"
+	echo; echo "Seguros ConductORT"; echo
+
+	echo "1) Registrar Matricula"
+	echo "2) Ver Matriculas Registradas"
+	echo "3) Buscar Matriculas por Usuario"
+	echo "4) Cambiar Permiso de Modificacion"
+	echo "5) Salir"; echo
+
+	read -p 'Seleccione una opcion: ' x
+	case $x in 
+		'1')	Registrar_Matricula
+			read -sp "Presione [ENTER] para continuar"
+			;;
+		'2')	Ver_Matriculas_Registradas
+			read -sp "Presione [ENTER] para continuar"
+			;;
+		'3') 	Buscar_Matriculas_por_Usuario
+			read -sp "Presione [ENTER] para continuar"
+			;;
+		'4')	Cambiar_Permiso_de_Modificacion
+			read -sp "Presione [ENTER] para continuar"
+			;;
+		'5')	echo "Saliendo..."; echo
+			break
+			;;
+		*)	echo "No es una opcion valida"; echo
+			read -sp "Presione [ENTER] para continuar"
+			;;
+	esac
+done
