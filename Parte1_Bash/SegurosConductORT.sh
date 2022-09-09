8a35217a18a3c02e85a9814db95eb75f2f55a8e4
TadeoGoday <tadeo.goday@gmail.com>
2022-09-08 22:04:50 -0300

    ADD: Funciones registrar matricula (falta terminar) y mostrar matriculas

--- Parte1_Bash/SegurosConductORT.sh
+++ Parte1_Bash/SegurosConductORT.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 
 mkdir logs 2>/dev/null #Crea carpeta para almacenar registros
-touch logs/registro_$(date +%F_%T).log #Genera archivo log con la fecha en su nombre
+log_file=logs/registro_$(date +%F_%T).log #Variable para referenciar al archivo log 
+touch $logFile #Genera archivo log con la fecha en su nombre
 
 #Funcion para: Registrar Matriculas
 Registrar_Matricula(){
-	echo; echo "Registrar Matricula"
-	echo "Funcion 1"; echo;
+	echo; echo "[+] Registrar Matricula"
+
+	#Pedir datos
+	read -p "Ingrese la matricula: " matricula
+	read -p "Ingrese la cedula del responsable: " cedula
+	read -p "Ingrese la fecha de vencimiento del seguro (YYYY-MM-DD): " fecha
+	echo -e "$matricula | $cedula | $fecha\n"
+
+	####### [falta verificar datos y calcular estado de vencimiento] #######
+	echo -e "Operacion exitosa!"
+
+	#Guarda matricula en el archivo matriculas.txt
+	echo "$matricula | $cedula | $fecha" >> matriculas.txt
 	
+	#Registrar en el log
+	echo -e "Operacion $(date +%T)\nRegistrar Matricula\n[ $matricula | $cedula | $fecha ]\n" >> $log_file
 }
 
 #Funcion para: Ver Matriculas Registradas
 Ver_Matriculas_Registradas(){
-	echo; echo "Ver Matriculas Registradas"
-	echo "Funcion 2"; echo;
-	
+	echo; echo "[+] Ver Matriculas Registradas"
+	echo -e "Matricula | Cedula | Estado" | column -t
+	cat matriculas.txt | column -t 
 }
 
 #Funcion para: Buscar Matriculas por Usuario
@@ -45,22 +59,18 @@ while [ true ]; do
 	read -p 'Seleccione una opcion: ' x
 	case $x in 
 		'1')	Registrar_Matricula
-			read -sp "Presione [ENTER] para continuar"
 			;;
 		'2')	Ver_Matriculas_Registradas
-			read -sp "Presione [ENTER] para continuar"
 			;;
 		'3') 	Buscar_Matriculas_por_Usuario
-			read -sp "Presione [ENTER] para continuar"
 			;;
 		'4')	Cambiar_Permiso_de_Modificacion
-			read -sp "Presione [ENTER] para continuar"
 			;;
 		'5')	echo "Saliendo..."; echo
 			break
 			;;
 		*)	echo "No es una opcion valida"; echo
-			read -sp "Presione [ENTER] para continuar"
 			;;
 	esac
+	echo; read -sp "Presione [ENTER] para continuar"
 done
