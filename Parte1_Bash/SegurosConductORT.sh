f19fe549139cc75d918de8b0b2f6f551ad3e09c4
Tadeo Goday <tadeo.goday@gmail.com>
2022-09-14 20:32:40 -0300

    FIX: Función Registrar Matricula con Regex

--- Parte1_Bash/SegurosConductORT.sh
+++ Parte1_Bash/SegurosConductORT.sh
@@ -2,8 +2,6 @@
 
 mkdir logs 2>/dev/null #Crea carpeta para almacenar registros
 log_file=logs/registro_$(date +%F_%T).log #Variable para referenciar al archivo log 
-# ¿Crear archivo?
-touch $log_file #Genera archivo log con la fecha en su nombre
 
 #Funcion para: Registrar Matriculas
 Registrar_Matricula(){
@@ -14,53 +12,23 @@ Registrar_Matricula(){
 		read -p "Ingrese la matricula: " matricula
 		matricula=$(echo $matricula | tr '[:lower:]' '[:upper:]')
 		#Verificar matricula
-		IFS='-' read -ra arr_matricula <<< "$matricula" #Creamos array de la matricula todo raro
-		letras=${arr_matricula[0]}
-		numeros=${arr_matricula[1]}
-		if [ ${#arr_matricula[@]} -eq 2 -a ${#letras} -eq 3 -a ${#numeros} -eq 4 ]; then #Verifica tamaños
-			if [ ! $(echo $letras | grep [0-9] | wc -l) -gt 0 -a $(echo $letras | grep "\bS\w*\b" | wc -l) -eq 1 ]; then #Verifica no tenga digitos y sea de montevideo en las letras
-				if [ ! $(echo $numeros | grep "[A-Z]" | wc -l) -gt 0  ]; then #Verifica no tenga letras en la parte numerica
-					break 
-				fi
-			fi
-		fi
+		if [ $(echo $matricula | grep -E "\bS[A-Z]{2}-[0-9]{4}\b" -c) -eq 1 ]; then break; fi
 		echo -e "Matricula Invalida\n"
 	done
 	
 	#Pedir cedula
 	while [ true ]; do
 		read -p "Ingrese la cédula del responsable: " cedula
-		cedula=$(echo $cedula | tr '[:lower:]' '[:upper:]')
 		#Verificar cedula
-		IFS='-' read -ra arr_cedula <<< "$cedula" #Creamos array de la cedula todo raro
-		digitos=${arr_cedula[0]}
-		verificador=${arr_cedula[1]}
-		if [ ${#arr_cedula[@]} -eq 2 -a ${#digitos} -eq 9 -a ${#verificador} -eq 1 ]; then #Verifica tamaños
-			IFS='.' read -ra arr_ced <<< "$digitos" #Creamos array de los digitos de la cedula todo raro
-			primer=${arr_ced[0]}
-			segundos=${arr_ced[1]}
-			terceros=${arr_ced[2]}
-			if [ ${#arr_ced[@]} -eq 3 -a ${#primer} -eq 1 -a ${#segundos} -eq 3 -a ${#terceros} -eq 3 ]; then #Verifica tamaños
-				break
-			fi
-		fi
+		if [ $(echo $cedula | grep -E "\b[0-9]{1}.[0-9]{3}.[0-9]{3}-[0-9]{1}\b" -c) -eq 1 ]; then break; fi
 		echo -e "Cédula Invalida\n"
 	done
 
 	#Pedir fecha
 	while [ true ]; do
 		read -p "Ingrese la fecha de vencimiento del seguro (YYYY-MM-DD): " fecha
-		fecha=$(echo $fecha | tr '[:lower:]' '[:upper:]')
 		#Verificar fecha
-		IFS='-' read -ra arr_fecha <<< "$fecha" #Creamos array de la fecha todo raro
-		ano=${arr_fecha[0]}
-		mes=${arr_fecha[1]}
-		dia=${arr_fecha[2]}
-		if [ ${#arr_fecha[@]} -eq 3 -a ${#ano} -eq 4 -a ${#mes} -le 2 -a ${#dia} -le 2 ]; then #Verifica tamaños
-			if [ $((dia)) -lt 32 -a $((dia)) -gt 0 -a $((mes)) -lt 13 -a $((mes)) -gt 0 ]; then #Verifica  dias y meses
-				break
-			fi
-		fi
+		if [ $(echo $fecha | grep -E "\b[0-9]{4}-([1-9]|0[1-9]|1[12])-([1-9]|[012][0-9]|3[01])\b" -c) -eq 1 ]; then break; fi
 		echo -e "Fecha Invalida\n"
 	done
 
